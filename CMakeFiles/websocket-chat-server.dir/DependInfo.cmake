eb9bd2d4007c73628951dad23b17377a9fabf264
Aryan Jha <aryanjha026@gmail.com>
2023-07-02 00:27:36 +0530

    Added index.html

---
+++ CMakeFiles/websocket-chat-server.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ryan_aa/Chat/http_session.cpp" "CMakeFiles/websocket-chat-server.dir/http_session.cpp.o" "gcc" "CMakeFiles/websocket-chat-server.dir/http_session.cpp.o.d"
+  "/home/ryan_aa/Chat/listener.cpp" "CMakeFiles/websocket-chat-server.dir/listener.cpp.o" "gcc" "CMakeFiles/websocket-chat-server.dir/listener.cpp.o.d"
+  "/home/ryan_aa/Chat/main.cpp" "CMakeFiles/websocket-chat-server.dir/main.cpp.o" "gcc" "CMakeFiles/websocket-chat-server.dir/main.cpp.o.d"
+  "/home/ryan_aa/Chat/shared_state.cpp" "CMakeFiles/websocket-chat-server.dir/shared_state.cpp.o" "gcc" "CMakeFiles/websocket-chat-server.dir/shared_state.cpp.o.d"
+  "/home/ryan_aa/Chat/websocket_session.cpp" "CMakeFiles/websocket-chat-server.dir/websocket_session.cpp.o" "gcc" "CMakeFiles/websocket-chat-server.dir/websocket_session.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/websocket-chat-server.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/websocket-chat-server.dir/http_session.cpp.o"
+  "CMakeFiles/websocket-chat-server.dir/http_session.cpp.o.d"
+  "CMakeFiles/websocket-chat-server.dir/listener.cpp.o"
+  "CMakeFiles/websocket-chat-server.dir/listener.cpp.o.d"
+  "CMakeFiles/websocket-chat-server.dir/main.cpp.o"
+  "CMakeFiles/websocket-chat-server.dir/main.cpp.o.d"
+  "CMakeFiles/websocket-chat-server.dir/shared_state.cpp.o"
+  "CMakeFiles/websocket-chat-server.dir/shared_state.cpp.o.d"
+  "CMakeFiles/websocket-chat-server.dir/websocket_session.cpp.o"
+  "CMakeFiles/websocket-chat-server.dir/websocket_session.cpp.o.d"
+  "websocket-chat-server"
+  "websocket-chat-server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/websocket-chat-server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

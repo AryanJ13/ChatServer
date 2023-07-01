2ac178b3a625d82f4ed029e1e258d12cad1bf25a
Aryan Jha <aryanjha026@gmail.com>
2023-07-01 20:26:28 +0530

    Uploading Project

---
+++ beast.hpp
@@ -0,0 +1,10 @@
+#ifndef CPPCON2018_BEAST_HPP
+#define CPPCON2018_BEAST_HPP
+
+#include <boost/beast.hpp>
+
+namespace beast = boost::beast;
+namespace http = boost::beast::http;            // from <boost/beast/http.hpp>
+namespace websocket = boost::beast::websocket;  // from <boost/beast/websocket.hpp>
+
+#endif
